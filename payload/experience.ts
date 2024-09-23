0447339081d5a4072edde7d4fc57bd1606db4d8a
siyoung choi <sy.choi@okestro.com>
2024-09-23 11:22:16 +0900

    update

--- payload/experience.ts
+++ payload/experience.ts
@@ -8,7 +8,10 @@ const experience: IExperience.Payload = {
       title: '오케스트로(주)',
       position: 'DevOps Engineer',
       startedAt: '2019-10',
-      descriptions: ['https://www.okestro.com/'],
+      descriptions: [
+          '클라우드 전환 및 설계 컨설팅부터 구축, 운영을 위한 다양한 솔루션을 자체 개발해 제공하는 기업',
+          'https://www.okestro.com/'
+      ],
       skillKeywords: [
         'Kubernetes',
         'Java',
