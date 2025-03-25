f7262a176b645afd7c37252ef0ab1b894f168aee
siyoung choi <sy.choi@okestro.com>
2025-03-25 18:15:39 +0900

    update

--- payload/presentation.ts
+++ payload/presentation.ts
@@ -4,17 +4,17 @@ const presentation: IPresentation.Payload = {
   disable: false,
 
   list: [
-    {
-      title: 'Kafka 분석',
-      subTitle: 'kafka의 구성 및 동작 원리를 사내 그룹 스터디를 통해 분석하였음.',
-      at: '2023-09',
-      descriptions: [
-        {
-          content: 'Slideshare:',
-          postHref: 'https://www.slideshare.net/Sapcy/kafka-261412533',
-        },
-      ],
-    },
+    // {
+    //   title: 'Kafka 분석',
+    //   subTitle: 'kafka의 구성 및 동작 원리를 사내 그룹 스터디를 통해 분석하였음.',
+    //   at: '2023-09',
+    //   descriptions: [
+    //     {
+    //       content: 'Slideshare:',
+    //       postHref: 'https://www.slideshare.net/Sapcy/kafka-261412533',
+    //     },
+    //   ],
+    // },
     {
       title: '사내 인터뷰',
       subTitle: '회사 블로그 업무 관련 인터뷰 참여.',
